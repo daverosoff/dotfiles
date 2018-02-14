243bed5f0a30f94a42e5b67e0bb31e79d74c8134
Dave Rosoff <drosoff@collegeofidaho.edu>
2018-02-14 15:00:07 -0700

    use k

--- antigen/antigen.zsh
+++ antigen/antigen.zsh
@@ -3,5 +3,6 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle mafredri/zsh-async
 antigen bundle sindresorhus/pure
 antigen bundle rupa/z
+antigen bundle supercrabtree/k
 antigen apply
 
